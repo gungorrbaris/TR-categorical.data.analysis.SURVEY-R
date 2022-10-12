8e6d30489c4dee05accb4692e948c90beb0e3681
Barış Güngör <65726895+gungorrbaris@users.noreply.github.com>
2022-10-12 20:25:05 +0300

    Add files via upload

---
+++ Rcode/packages.R
@@ -0,0 +1,45 @@
+
+
+## ---------------------------
+library(dplyr)
+library(ggplot2)
+library(plotrix)
+library(ggridges)
+library(ggpol)
+library(hrbrthemes)
+library(heatmaply)
+library(RColorBrewer)
+library(ggsci)
+library(wesanderson)
+library(viridis)
+library(tidygraph)
+library(ggpubr)
+library(ggthemes)
+
+
+## ---------------------------
+library(DescTools)
+library(vcd)
+library(haven)
+library(ca)
+library(FactoMineR)
+library(rcompanion)
+library(factoextra)
+
+
+## ---------------------------
+library(vcdExtra)
+
+
+## ---------------------------
+library(vcdExtra)
+
+
+## ---------------------------
+library(car)
+library(ResourceSelection)
+library(MASS)
+library(generalhoslem)
+
+
+
